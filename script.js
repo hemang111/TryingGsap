5c3ef15be182877ff580685c7ad18cc3575c835b
Hemang Choudhary <hemangc37@gmail.com>
2024-04-18 20:59:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var tl = gsap.timeline();
+document.getElementById("nav").addEventListener("click", function(){
+    gsap.from("#nav h3",{
+        y: -50,
+        opacity: 0,
+        delay : 0.3,
+        duration:1,
+        easing: "easeInOutQuad",
+        stagger: 0.3,
+    }) 
+})
+
+tl.from("#nav h3",{
+    y: -50,
+    opacity: 0,
+    delay : 0.3,
+    duration:0.8,
+    easing: "easeInOutQuad",
+    stagger: 0.3,
+})
+tl.from("img",{
+    x: 100,
+    rotate : 45,
+    opacity:0,
+    scale:1,
+    duration:0.5,
+    easing: "easeInOutQuad",
+    stagger:0.8,
+})
+tl.to("img",{
+    scale:0.6,
+    opacity:1,
+    duration:1,
+    filter: "blur(10px)"
+})
+tl.from("#main p",{
+    x:-500,
+    opacity: 0,
+    delay : 0.3,
+    duration: 0.8,
+    easing: "easeInOutQuad",
+    stagger: 0.4,
+})
+
